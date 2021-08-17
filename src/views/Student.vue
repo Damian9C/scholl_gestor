1f1457aaee5c98a83eb1cc5c18d907e42987f5ab
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-08-17 13:05:51 -0500

    vista de reportes terminada

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -32,41 +32,20 @@
 
           <div class="studentCmp__reports--content">
             <v-virtual-scroll
-                :items="items"
-                :item-height="60"
+                :items="student.reports"
+                :item-height="120"
                 height="450px"
             >
               <template v-slot:default="{ item }">
+                <v-divider/>
                 <v-list-item>
-                  <v-list-item-avatar>
-                    <v-avatar
-                        :color="item.color"
-                        size="56"
-                        class="white--text"
-                    >
-                      {{ item.initials }}
-                    </v-avatar>
-                  </v-list-item-avatar>
-
                   <v-list-item-content>
-                    <v-list-item-title>{{ item.fullName }}</v-list-item-title>
+                    <div class="studentCmp__reports--contentItem">
+                      <div class="studentCmp__reports--contentTitle">{{item.teacher}}</div>
+                      <div class="studentCmp__reports--contentDate">{{generateDate(item.date)}}</div>
+                    </div><br/><br/>
+                    <p class="studentCmp__reports--contentTxt">{{item.incident}}</p>
                   </v-list-item-content>
-
-                  <v-list-item-action>
-                    <v-btn
-                        depressed
-                        small
-                    >
-                      View User
-
-                      <v-icon
-                          color="orange darken-4"
-                          right
-                      >
-                        mdi-open-in-new
-                      </v-icon>
-                    </v-btn>
-                  </v-list-item-action>
                 </v-list-item>
               </template>
             </v-virtual-scroll>
@@ -90,16 +69,15 @@ export default {
   components: {Return_bar, General, Information, Search_bar},
   data: () => ({
     student:{},
-    colors: ['#2196F3', '#90CAF9', '#64B5F6', '#42A5F5', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1', '#82B1FF', '#448AFF', '#2979FF', '#2962FF'],
-    names: ['Oliver', 'Jake', 'Noah', 'James', 'Jack', 'Connor', 'Liam', 'John', 'Harry', 'Callum', 'Mason', 'Robert', 'Jacob', 'Jacob', 'Jacob', 'Michael', 'Charlie', 'Kyle', 'William', 'William', 'Thomas', 'Joe', 'Ethan', 'David', 'George', 'Reece', 'Michael', 'Richard', 'Oscar', 'Rhys', 'Alexander', 'Joseph', 'James', 'Charlie', 'James', 'Charles', 'William', 'Damian', 'Daniel', 'Thomas', 'Amelia', 'Margaret', 'Emma', 'Mary', 'Olivia', 'Samantha', 'Olivia', 'Patricia', 'Isla', 'Bethany'],
-    surnames: ['Smith', 'Anderson', 'Clark', 'Wright', 'Mitchell', 'Johnson', 'Thomas', 'Rodriguez', 'Lopez', 'Perez', 'Williams', 'Jackson', 'Lewis', 'Hill', 'Roberts', 'Jones', 'White', 'Lee', 'Scott', 'Turner', 'Brown', 'Harris', 'Walker', 'Green', 'Phillips', 'Davis', 'Martin', 'Hall', 'Adams', 'Campbell', 'Miller', 'Thompson', 'Allen', 'Baker', 'Parker', 'Wilson', 'Garcia', 'Young', 'Gonzalez', 'Evans', 'Moore', 'Martinez', 'Hernandez', 'Nelson', 'Edwards', 'Taylor', 'Robinson', 'King', 'Carter', 'Collins'],
   }),
 
   methods:{
-    genRandomIndex (length) {
-      return Math.ceil(Math.random() * (length - 1))
-    },
+    generateDate(date){
+      let dateFormat = new Date(Number(date.seconds + "000"))
+      let dateConstructor = `${dateFormat.getDay()}/${dateFormat.getMonth()}/${dateFormat.getFullYear()}`
 
+      return dateConstructor
+    },
 
     async getData(){
       let request = await db.collection('groups')
@@ -117,29 +95,11 @@ export default {
             group: "1.-A",
             numControl: e.id,
             medicalCondition: e.medicalCondition,
+            reports: e.reports,
           };
         }
       });
     }
-  },
-
-  computed: {
-    items () {
-      const namesLength = this.names.length
-      const surnamesLength = this.surnames.length
-      const colorsLength = this.colors.length
-
-      return Array.from({ length: 100 }, (k, v) => {
-        const name = this.names[this.genRandomIndex(namesLength)]
-        const surname = this.surnames[this.genRandomIndex(surnamesLength)]
-
-        return {
-          color: this.colors[this.genRandomIndex(colorsLength)],
-          fullName: `${name} ${surname}`,
-          initials: `${name[0]} ${surname[0]}`,
-        }
-      })
-    },
   },
 
   mounted() {
@@ -169,6 +129,23 @@ export default {
 .studentCmp__reports--title{
   display: flex;
   justify-content: space-between;
+}
+
+.studentCmp__reports--contentItem{
+  display: flex;
+}
+
+.studentCmp__reports--contentTitle{
+  margin-top: .5rem;
+  font-size: 1.1rem;
+}
+
+.studentCmp__reports--contentDate{
+  font-size: .8rem;
+}
+
+.studentCmp__reports--contentTxt{
+  font-size: .95rem;
 }
 
 
