9fa24ed5b506dcfc0f4789ac11fe58997fd5e367
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-08-20 11:52:25 -0500

    cambio de contraseña añadido

--- src/components/navigationBars/navigation_bar.vue
+++ src/components/navigationBars/navigation_bar.vue
@@ -36,6 +36,19 @@
         <v-card>
           <v-list>
             <v-list-item-group>
+              <v-list-item @click.prevent="showChangePass = true">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Cambiar Contraseña
+                  </v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-icon>
+                  <v-icon>
+                    mdi-lock-reset
+                  </v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+
               <v-list-item @click.prevent="logOut">
                 <v-list-item-content>
                   <v-list-item-title>
@@ -62,17 +75,108 @@
       </v-avatar>
     </div>
 
+    <div class="navBar__showChangePass">
+      <v-dialog
+          v-model="showChangePass"
+          width="35vw"
+          persistent
+      >
+        <template v-slot:activator="{on, attrs}"/>
+
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Cambiar Contraseña
+          </v-card-title><br/>
+
+          <div class="navBar__showChangePass--content">
+            <v-text-field
+                v-model="password"
+                outlined
+                append-icon="mdi-lastpass"
+                :rules="[rules.required]"
+                type="password"
+                name="input-10-1"
+                label="Nueva Contraseña"
+            ></v-text-field>
+
+            <v-text-field
+                v-model="repeatPassword"
+                outlined
+                append-icon="mdi-lastpass"
+                :rules="[rules.required]"
+                type="password"
+                name="input-10-1"
+                label="Repetir Contraseña"
+            ></v-text-field>
+          </div>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="red"
+                text
+                @click="showChangePass = false"
+            >
+              Cancelar
+            </v-btn>
+            <v-btn
+                dark
+                color="#51B4E9"
+                @click="changePass"
+            >
+              Actualizar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog
+          v-model="showAlert"
+          width="35vw"
+          persistent
+      >
+        <template v-slot:activator="{on, attrs}"/>
+
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Contraseña Guardada
+          </v-card-title><br/>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                dark
+                color="#51B4E9"
+                @click="showAlert = false"
+            >
+              Ok
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+
   </v-app-bar>
 </template>
 
 <script>
 import {auth} from "../../util";
 import {mapActions} from "vuex";
+import {updatePass} from "../../util/utilities";
 
 export default {
   name: "navigation_bar",
 
   data: () => ({
+    showChangePass:'',
+    showAlert:'',
+
+    rules: {
+      required: value => !!value || 'Obligatorio'
+    },
+    password: '',
+    repeatPassword:'',
+
     text: '',
     colors:{
       admin: '#FF097F',
@@ -90,6 +194,18 @@ export default {
       auth.signOut().then(() => {
         this.$router.push('/');
       })
+    },
+
+     async changePass(){
+       if (this.password === this.repeatPassword){
+         await updatePass(this.password);
+         console.log('Actualizado')
+
+         this.showChangePass = false;
+         this.showAlert = true;
+       }else {
+         alert('Las Contraseñas no Coinciden')
+       }
     },
 
     setNick (){
@@ -112,6 +228,10 @@ export default {
 
 .navBar__user{
   margin-left: .2rem;
+}
+
+.navBar__showChangePass--content{
+  padding: 0 5%;
 }
 
 </style>
--- src/util/utilities.js
+++ src/util/utilities.js
@@ -1,5 +1,14 @@
-import {db} from "./index";
+import {auth, db} from "./index";
 
 export async function getStudents(id) {
     return await db.collection('groups').where('group','==', id).get();
+}
+
+export async function updatePass(newPass) {
+    try {
+        const user = auth.currentUser;
+        await user.updatePassword(newPass)
+    }catch (e) {
+        alert(e)
+    }
 }
